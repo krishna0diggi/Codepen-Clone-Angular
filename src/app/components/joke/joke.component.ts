85aee96877ecb0f18263b494c0d9292d2341df84
Krishna Kumar Saw <krishna.kumarsaw@diggibyte.com>
2024-10-01 16:06:57 +0530

    service-file-updtaed

---
+++ src/app/components/joke/joke.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-joke',
+  standalone: true,
+  imports: [],
+  templateUrl: './joke.component.html',
+  styleUrl: './joke.component.css'
+})
+export class JokeComponent {
+
+}
