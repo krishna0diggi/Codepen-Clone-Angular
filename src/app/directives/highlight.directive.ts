2f3154805782de479c5df5d10956fef224a72ffb
Krishna Kumar Saw <krishna.kumarsaw@diggibyte.com>
2024-10-01 14:26:44 +0530

    hostListener

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
@@ -6,9 +6,21 @@ import { Directive, HostBinding } from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(public el: ElementRef) { }
 
   @HostBinding(`style.backgroundColor`) 
   bgColor:string = 'magenta'
+
+  @HostListener("mouseenter")
+  changeFontSize(){
+    console.log("Mouse Enter")
+    this.el.nativeElement.style.fontSize = "50px"
+  }
+
+  @HostListener("mouseleave")
+  resetFontSize(){
+    console.log("Mouse leave")
+    this.el.nativeElement.style.fontSize = "20px"
+  }
 
 }
