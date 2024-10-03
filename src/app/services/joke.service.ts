d1442ecb6486f9332e09ecfd511e38f0f791b35f
Krishna Kumar Saw <krishna.kumarsaw@diggibyte.com>
2024-10-03 15:00:16 +0530

    joke-api

--- src/app/services/joke.service.ts
+++ src/app/services/joke.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JokeService {
+  http:HttpClient
 
-  constructor() { }
+  constructor(http:HttpClient) {
+    this.http = http
+   }
+
+   getJoke() {
+    
+   }
 }
