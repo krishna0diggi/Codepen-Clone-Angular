2a6903be526a4ec2901b3f3351a8bd020b1af7e7
Krishna Kr Saw <krishna.kumarsaw@diggibyte.com>
2024-10-07 15:46:34 +0530

    frontend-complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
     {path: 'signup', component: SignupComponent},
+
+    // Without lazy loading
+    // {path: 'about', component: AboutComponent},
+
+    // With Lazy Loading
+    {path: 'about', loadComponent: () => import('./components/about/about.component').then(mod=> mod.AboutComponent)},
     {path: '', redirectTo:'/login', pathMatch:"full"},
 
     // wild card
