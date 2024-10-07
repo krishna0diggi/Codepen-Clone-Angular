cee0988aabc27165293c6799d583da7252375167
Krishna Kr Saw <krishna.kumarsaw@diggibyte.com>
2024-10-07 15:07:45 +0530

    signup-and-404-page-added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
-    {path: 'signup', component: SignupComponent}
+    {path: 'signup', component: SignupComponent},
+    {path: '', redirectTo:'/login', pathMatch:"full"},
+
+    // wild card
+    {path: '**', component:NotFoundComponent}
 ];
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+
+  constructor(private router:Router){}
+  
+  register(regForm:NgForm){
+    console.log(regForm.value)
+    this.router.navigate(['/login'])
+  }
+  reset(regForm:NgForm)
+  {
+    console.log(regForm.reset())
+  }
 
 }
