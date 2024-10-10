575cc5caab371b3725ace08fcb20ec8261eef6b3
Krishna Kr Saw <krishna.kumarsaw@diggibyte.com>
2024-10-10 12:45:11 +0530

    ngAfterViewInit-input-state-change

--- src/app/components/observable/observable.component.ts
+++ src/app/components/observable/observable.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { map,Observable, toArray, tap } from 'rxjs';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { map,Observable, toArray, tap, fromEvent } from 'rxjs';
 
 interface Profile {
   name: string;
@@ -18,8 +18,16 @@ interface Profile {
 
 export class ObservableComponent implements OnInit{
 
+  // @ViewChild('search', {static: true}) serach? = ElementRef<HTMLInputElement>
+  @ViewChild('search') searchInput?:ElementRef<HTMLInputElement>;
+  // profileData$:Observable<Profile[]>
+
   ngOnInit(){
     const profileData = new Observable<Profile>((subscriber)=> {
+
+      // fromEvent(this.serach!.nativeElement, "input")
+
+
       subscriber.next({ name: "Jack", position: "Full Stack Developer", experience: 3 });
       subscriber.next({ name: "Bob", position: "Jr UI/UX Designer", experience: 1 });
       subscriber.next({ name: "Devin", position: "Backend Developer", experience: 2 });
@@ -41,6 +49,16 @@ export class ObservableComponent implements OnInit{
       next: (names)=> console.log(names),
       complete: () => console.log('Completed')
     })
+  }
+
+  ngAfterViewInit() {
+    if(this.searchInput)
+    {
+      fromEvent(this.searchInput.nativeElement, "input").subscribe((event:Event)=>{
+        const input = (event.target as HTMLInputElement).value;
+        console.log("INput Value", input)
+      })
+    }
   }
  
 }
